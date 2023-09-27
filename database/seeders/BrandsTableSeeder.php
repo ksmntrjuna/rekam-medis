b75c7d356eaf8b2a9a12502d52a0f884c62317a4
BHIIII <dobenk81@gmail.com>
2023-09-27 14:49:21 +0700

    modified seeder

---
+++ database/seeders/BrandsTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class BrandsTableSeeder extends Seeder
+{
+
+    /**
+     * Auto generated seed file
+     *
+     * @return void
+     */
+    public function run()
+    {
+      
+        \DB::table('brands')->delete();
+        
+        \DB::table('brands')->insert(array (
+            0 => 
+            array (
+                'created_at' => '2020-12-04 08:07:52',
+                'id' => '1',
+                'name' => 'Navagreen',
+                'updated_at' => '2020-12-04 08:07:52',
+            ),
+            1 => 
+            array (
+                'created_at' => '2020-12-04 08:07:52',
+                'id' => '2',
+                'name' => 'Natasha',
+                'updated_at' => '2020-12-04 08:07:52',
+            ),
+        ));
+    }
+}
